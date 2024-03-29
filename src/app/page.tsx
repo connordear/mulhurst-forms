eecba38f0f2c32500ee6722ac1b37ed540992196
Connor Dear <connordear@gmail.com>
2024-03-29 17:46:56 -0600

    added page titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 import MullyForm from "@/components/MullyForm";
 import { Card, CardHeader } from "@/components/ui/card";
+import Head from "next/head";
 
 export default async function Home() {
   return (
-    <main className="md:flex min-h-screen flex-col items-center md:p-24 p-10 bg-gradient">
-      <Card className="mb-10">
-        <CardHeader>
-          <h1 className="text-4xl font-bold color-white text-center">
-            Mulhurst Camp Registration Form
-          </h1>
-        </CardHeader>
-      </Card>
-      <MullyForm />
-    </main>
+    <div>
+      <Head>
+        <title>Mulhurst Camp Registration Form</title>
+      </Head>
+      <main className="md:flex min-h-screen flex-col items-center md:p-24 p-10 bg-gradient">
+        <Card className="mb-10">
+          <CardHeader>
+            <h1 className="text-4xl font-bold color-white text-center">
+              Mulhurst Camp Registration Form
+            </h1>
+          </CardHeader>
+        </Card>
+        <MullyForm />
+      </main>
+    </div>
   );
 }
--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { LoadingPage } from "@/components/LoadingCard";
+import Head from "next/head";
 import { Suspense } from "react";
 import SubmitCard from "./SubmitCard";
 
 const SubmitPage = () => {
   return (
     <Suspense fallback={<LoadingPage />}>
+      <Head>
+        <title>Mulhurst Camp Registration Form</title>
+      </Head>
       <SubmitCard />
     </Suspense>
   );
